e03c6b8dd9b1803ac9f1dda0cf21cad7ae5fe94d
Naresh Kumar <nk2indian@gmail.com>
2020-10-18 09:30:05 +0530

    Program at initial stage

---
+++ q2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class q2 
+{
+	int input1,input2;
+	class Calculate
+	{
+		void getvalues()
+		{
+			Scanner ip = new Scanner(System.in);
+			System.out.print("Input1 = ");
+		}
+	}
+}
