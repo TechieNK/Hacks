12ef12b5fa0a1b24c3d4e31a8c06e202f4bc4bb8
Naresh Kumar <nk2indian@gmail.com>
2020-10-18 10:13:10 +0530

    q1 started

---
+++ q1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class q1 
+{
+	public static void main(String[] args)
+	{
+		Scanner ip = new Scanner(System.in);
+		String s = ip.nextLine();
+		String[] arr = s.split(" ");
+		int len = arr.length;
+	}
+}
